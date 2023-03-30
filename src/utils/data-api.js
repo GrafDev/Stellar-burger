108da1abb510910c786e5f945e134720d0695772
Graf <65903163+GrafDev@users.noreply.github.com>
2023-03-30 09:57:02 +0300

    I try to Added load data from server. -> it is mistake

--- src/utils/data-api.js
+++ src/utils/data-api.js
@@ -1,19 +1,16 @@
 const DATA_URL = 'https://norma.nomoreparties.space/api/ingredients';
 
-const checkResponse = (res) => {
-	return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
-};
 
-//
-// const getResponse=(res)=>{
-// 	if (res.ok){
-// 		console.log(res.json(),'-res.ok')
-//
-// 		return res.json();
-// 	}
-// 	return Promise.reject(`Error ${res.status}`);
-// }
+
+const getResponse=(res)=>{
+	if (res.ok){
+		console.log(res.json(),'-res.ok')
+
+		return res.json();
+	}
+	return Promise.reject(`Error ${res.status}`);
+}
 
 export const getData = async () => {
-	return fetch(DATA_URL).then(checkResponse);
+	return fetch(DATA_URL).then(getResponse);
 }
