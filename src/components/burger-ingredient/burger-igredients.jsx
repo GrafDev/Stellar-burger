d63f05d0c182945ba4a8f68dce50632eae1e2b96
Graf <65903163+GrafDev@users.noreply.github.com>
2023-04-03 22:40:44 +0300

    Make switch tabs

--- src/components/burger-ingredient/burger-igredients.jsx
+++ src/components/burger-ingredient/burger-igredients.jsx
@@ -11,7 +11,7 @@ import {useRect} from "../../hoc/userect";
 
 function BurgerIgredients() {
 	const data= useSelector(getIngredients)
-	const {currentType,setCurrentType}=useState('bun');
+	const [currentType,setCurrentType]=useState('bun')
 	const bunRef=useRef();
 	const mainRef=useRef();
 	const sauceRef=useRef();
@@ -19,34 +19,48 @@ function BurgerIgredients() {
 
 
 
-	useEffect(() => {
-
-		console.log(bunRef,bunRef.current)
-		console.log(mainRef,mainRef.current)
-		console.log(sauceRef,sauceRef.current)
-		console.log(tabRef,tabRef.current)
-	}, []);
-
-
 	let burgers = data;
+	// const changeTab=(type)=>{
+	// 	setCurrentType(type)
+	// }
 
-	function handlerScroll(e) {
-		tabRef.current.getBoundingClientRect()
+
+	const handlerScroll=()=> {
+		let _currentType;
+		let bottomTab=tabRef.current.getBoundingClientRect().bottom
+		let dimBun=Math.abs(bottomTab-bunRef.current.getBoundingClientRect().top)
+		let dimMain=Math.abs(bottomTab-mainRef.current.getBoundingClientRect().top)
+		let dimSauce=Math.abs(bottomTab-sauceRef.current.getBoundingClientRect().top)
+		if (dimBun<dimSauce) _currentType='bun'
+		else if (dimSauce<dimMain) _currentType='sauce'
+		else _currentType='main'
+		console.log(dimBun,dimMain,dimSauce)
+		 setCurrentType(_currentType)
+
+		// if(dimBun<0 && )
+		// if (bunRef.current.getBoundingClientRect().top-shareRef.current.getBoundingClientRect().bottom<32){
+		// 	console.log('bun')
+		// };
+		// if (mainRef.current.getBoundingClientRect().top-shareRef.current.getBoundingClientRect().bottom<32){
+		// 	console.log('main')
+		// };
+		// if (sauceRef.current.getBoundingClientRect().top-shareRef.current.getBoundingClientRect().bottom<32){
+		// 	console.log('sauce')
+		// };
 	}
 
-console.log(tabRef.current)
 
 	return (
 		<div className={styles.section} >
 			<div className={`${styles.title} ${textLarge}`}>Соберите бургер</div>
-			<div ref={tabRef} className={styles.tab}><BurgerTab current={currentType}/></div>
+			<div ref={tabRef}  className={styles.tab}><BurgerTab current={currentType} /></div>
 			<div className={styles.ingredients} onScroll={handlerScroll}>
 				<div ref={bunRef} className={`${textMedium} mb-10`}>Булки</div>
 				<IngredientCarts data={data} type={'bun'} bill={burgers} />
-				<div ref={mainRef} className={`${textMedium} mb-10`}>Начинка</div>
-				<IngredientCarts data={data} type={'main'} bill={burgers} />
 				<div ref={sauceRef} className={`${textMedium} mb-10`}>Соусы</div>
 				<IngredientCarts data={data} type={'sauce'} bill={burgers} />
+				<div ref={mainRef} className={`${textMedium} mb-10`}>Начинка</div>
+				<IngredientCarts data={data} type={'main'} bill={burgers} />
 			</div>
 		</div>
 	)
--- src/components/burger-ingredient/burger-tub/burger-tab.jsx
+++ src/components/burger-ingredient/burger-tub/burger-tab.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import {Tab} from "@ya.praktikum/react-developer-burger-ui-components";
+import PropTypes from "prop-types";
+import {typeCart} from "../../../utils/types";
+import IngredientCarts from "../ingredient-carts/ingredient-carts";
 
 
-function BurgerTab(){
-    const [current,setCurrent] = React.useState('bun')
-
+function BurgerTab(props){
+    const current=props.current
 
     return (
         <div style={{ display: 'flex' }}>
-            <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
+            <Tab value="bun" active={current === 'bun'} >
                 Булки
             </Tab>
-            <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
+            <Tab value="sauce" active={current === 'sauce'} >
                 Соусы
             </Tab>
-            <Tab value="main" active={current === 'main'} onClick={setCurrent}>
+            <Tab value="main" active={current === 'main'} >
                 Начинки
             </Tab>
         </div>
     )
+}
+
+BurgerTab.propTypes={
+    current:PropTypes.string.isRequired,
 }
 export  default BurgerTab;
