1c7a17fe86bf97d3265dc885dba35e5dc7dfd9d0
GREGORY IAKOVLEV <106439877+Bocxoq@users.noreply.github.com>
2023-06-05 00:22:50 +0300

    added login and register page

--- src/pages/forgot-password-page/forgot-password-page.jsx
+++ src/pages/forgot-password-page/forgot-password-page.jsx
@@ -1,8 +1,48 @@
+import {Button, EmailInput} from "@ya.praktikum/react-developer-burger-ui-components";
+import AuthorizationButton from "../../components/authorization-button/authorization-button";
+import {LOGIN_LINK} from "../../utils/constants/router-link-constants";
+import useForm from "../../hooks/useForm";
+
 const ForgotPasswordPage=()=>{
-    return(
-        <>
-            Forgot
-        </>
+
+    const { form, handleForm } = useForm({ email: '' })
+
+    const submitForm = (e) => {
+            e.preventDefault()
+        }
+
+
+    return (
+        <main className="container auth">
+            <h3 className="auth__title">Восстановление пароля</h3>
+
+            <form className="auth__form" onSubmit={submitForm}>
+                <EmailInput
+                    name="email"
+                    placeholder="Укажите e-mail"
+                    value={form.email}
+                    onChange={handleForm}
+                    autoFocus
+                />
+
+                <Button
+                    htmlType="submit"
+                    type="primary"
+                    size="large"
+                    disabled={!form.email}
+                >
+                    Восстановить
+                </Button>
+            </form>
+
+            <div className="auth__links">
+                <AuthorizationButton
+                    title="Вспомнили пароль?"
+                    buttonName="Войти"
+                    path={LOGIN_LINK}
+                />
+            </div>
+        </main>
     )
 }
 export default ForgotPasswordPage;
--- src/pages/reset-password-page/reset-password-page.jsx
+++ src/pages/reset-password-page/reset-password-page.jsx
@@ -1,8 +1,63 @@
+import {Button, Input, PasswordInput} from "@ya.praktikum/react-developer-burger-ui-components";
+import AuthorizationButton from "../../components/authorization-button/authorization-button";
+import useForm from "../../hooks/useForm";
+import {LOGIN_LINK} from "../../utils/constants/router-link-constants";
+
 const ResetPasswordPage=()=>{
-    return(
-        <>
-            Reset Password
-        </>
+
+    const { form, handleForm } = useForm({
+        password: '',
+        token: '',
+    })
+
+    const submitForm =
+        (e) => {
+            e.preventDefault()
+
+        }
+
+    return (
+        <main className="container auth">
+            <h3 className="auth__title">Восстановление пароля</h3>
+
+            <form className="auth__form" onSubmit={submitForm}>
+                <PasswordInput
+                    name={'password'}
+                    value={form.password}
+                    onChange={handleForm}
+                    placeholder="Введите новый пароль"
+                    autoFocus
+                />
+
+                <Input
+                    name="token"
+                    value={form.token}
+                    onChange={handleForm}
+                    type="text"
+                    placeholder="Введите код из письма"
+                    error={false}
+                    errorText="Ошибка"
+                    size="default"
+                />
+
+                <Button
+                    htmlType="submit"
+                    type="primary"
+                    size="large"
+                    disabled={!form.password || !form.token}
+                >
+                    Восстановить
+                </Button>
+            </form>
+
+            <div className="auth__links">
+                <AuthorizationButton
+                    title="Вспомнили пароль?"
+                    buttonName="Войти"
+                    path={LOGIN_LINK}
+                />
+            </div>
+        </main>
     )
 }
 export default ResetPasswordPage;
